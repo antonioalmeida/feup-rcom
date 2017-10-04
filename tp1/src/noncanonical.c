e04e26a8aa69b5707654ba6720021db1ad89b847
root <root@bancada5.netlab.fe.up.pt>
2017-10-04 17:28:17 +0100

    Added read functional

--- tp1/src/noncanonical.c
+++ tp1/src/noncanonical.c
@@ -11,89 +11,91 @@
 #define FALSE 0
 #define TRUE 1
 
+#define FLAG_RCV 0x7e
+#define A_RCV 0x03
+
 volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
-	int fd,c, res;
-	struct termios oldtio,newtio;
-	char buf[255];
+    int fd,c, res;
+    struct termios oldtio,newtio;
+    char buf[255];
 
-	if ( (argc < 2) ||
-			((strcmp("/dev/ttyS0", argv[1])!=0) &&
-					(strcmp("/dev/ttyS1", argv[1])!=0) )) {
-		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-		exit(1);
-	}
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
 
 
-	/*
+  /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
-	 */
+  */
+  
+    
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
 
-	fd = open(argv[1], O_RDWR | O_NOCTTY );
-	if (fd <0) {perror(argv[1]); exit(-1); }
 
-	if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-		perror("tcgetattr");
-		exit(-1);
-	}
-
-	bzero(&newtio, sizeof(newtio));
-	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-	newtio.c_iflag = IGNPAR;
-	newtio.c_oflag = 0;
-
-	/* set input mode (non-canonical, no echo,...) */
-	newtio.c_lflag = 0;
-
-	newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-	newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
-
-
-
-	/*
+  /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
-	 */
+    leitura do(s) prï¿½ximo(s) caracter(es)
+  */
 
 
 
-	tcflush(fd, TCIOFLUSH);
+    tcflush(fd, TCIOFLUSH);
 
-	if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-		perror("tcsetattr");
-		exit(-1);
-	}
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
 
-	printf("New termios structure set\n");
+    printf("New termios structure set\n");
 
+    char cenas[255];
+	
+    while (STOP==FALSE) {       /* loop for input */
+      res = read(fd,buf,255);   /* returns after 5 chars have been input */
+      buf[res]=0;               /* so we can printf... */
+      printf(":%s:%d\n", buf, res);
+      if (buf[res-1]=='\0') STOP=TRUE;
+	strcat(cenas,buf);
+    }
 
-	char result[255];
+    printf("Message: %s.\n", cenas);
+    cenas[strlen(cenas)+1]='\0';
+    res = write(fd,cenas,strlen(cenas)+1);   
+    printf("%d bytes written\n", res);
 
-	while (STOP==FALSE) {       /* loop for input */
-		res = read(fd,buf,255);   /* returns after 1 chars have been input */
-		if(res < 0) printf("ERROR: Cannot read from serial port!\n");
-		buf[res]=0;               /* so we can printf... */
-		//printf(":%s:%d\n", buf, res);
-		if (buf[res-1]=='\0') STOP=TRUE;
-		strcat(result,buf);
-	}
-
-	printf("%s\n", result);
-
-	res = write(fd,result,255);
-	if(res < 0) printf("ERROR: Cannot write to serial port!\n");
-	printf("%d bytes written\n", res);
-
-	/*
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião 
-	 */
+  /* 
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guiï¿½o 
+  */
 
 
-	tcsetattr(fd,TCSANOW,&oldtio);
-	close(fd);
-	return 0;
+
+    tcsetattr(fd,TCSANOW,&oldtio);
+    close(fd);
+    return 0;
 }
+
