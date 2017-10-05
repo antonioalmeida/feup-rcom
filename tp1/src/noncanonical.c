8796719fc84ed71cc6f1adb06418e66e63bdd92b
root <root@bancada2.netlab.fe.up.pt>
2017-10-05 17:36:41 +0100

    Updated TP1

--- tp1/src/noncanonical.c
+++ tp1/src/noncanonical.c
@@ -5,90 +5,123 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
 
-#define FLAG_RCV 0x7e
-#define A_RCV 0x03
-
 volatile int STOP=FALSE;
 
-int main(int argc, char** argv)
-{
-	int fd,c, res;
+
+int set_serialPort(int argc, char** argv, struct termios* oldtio, struct termios* newtio);
+int reset_serialPort(int fd, struct termios* oldtio);
+int read_write_message(int fd);
+
+int main(int argc, char** argv) {
+
 	struct termios oldtio,newtio;
-	char buf[255];
+	int error = 0;
+	//Set the serial port to right config
+	int fd=set_serialPort(argc, argv,&oldtio,&newtio);
+	if(fd != 1 || fd != -1 ){
+		printf("New termios structure set\n");
+	}else{
+		exit(fd);
+	}
+
+	//Ready do read a message and send back
+	if((error = read_write_message(fd)) == -1)
+		exit(error);
+
+	if(reset_serialPort(fd,&oldtio)==-1)
+		exit(-1);
+
+	close(fd);
+	return 0;
+}
+
+int set_serialPort(int argc, char** argv, struct termios* oldtio, struct termios* newtio){
+	int fd;
 
 	if ( (argc < 2) ||
 			((strcmp("/dev/ttyS0", argv[1])!=0) &&
 					(strcmp("/dev/ttyS1", argv[1])!=0) )) {
-		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-		exit(1);
+		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS0\n");
+		return 1;
 	}
 
 	/*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
+	  Open serial port device for reading and writing and not as controlling tty
+	  because we don't want to get killed if linenoise sends CTRL-C.
 	 */
 
-
-	fd = open(argv[1], O_RDWR | O_NOCTTY );
-	if (fd <0) {perror(argv[1]); exit(-1); }
-
-	if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-		perror("tcgetattr");
-		exit(-1);
+	if ((fd = open(argv[1], O_RDWR | O_NOCTTY )) <0) {
+		perror(argv[1]);
+		return -1;
 	}
 
-	bzero(&newtio, sizeof(newtio));
-	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-	newtio.c_iflag = IGNPAR;
-	newtio.c_oflag = 0;
+	if ( tcgetattr(fd,oldtio) == -1) { /* save CURRENT_STATE port settings */
+		perror("tcgetattr");
+		return -1;
+	}
+
+	bzero(newtio, sizeof(*newtio));
+	(*newtio).c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	(*newtio).c_iflag = IGNPAR;
+	(*newtio).c_oflag = 0;
 
 	/* set input mode (non-canonical, no echo,...) */
-	newtio.c_lflag = 0;
+	(*newtio).c_lflag = 0;
 
-	newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-	newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+	(*newtio).c_cc[VTIME]    = 0;   /* inter-character timer unused */
+	(*newtio).c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
 	/*
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) pr�ximo(s) caracter(es)
+	  VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+	  leitura do(s) proximo(s) caracter(es)
 	 */
 
 	tcflush(fd, TCIOFLUSH);
 
-	if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+	if ( tcsetattr(fd,TCSANOW,newtio) == -1) {
 		perror("tcsetattr");
-		exit(-1);
+		return -1;
 	}
+	return fd;
+}
 
-	printf("New termios structure set\n");
+int reset_serialPort(int fd,struct termios* oldtio){
+	if (tcsetattr(fd,TCSANOW,oldtio) == -1) {
+		perror("tcsetattr");
+		return -1;
+	}
+	return 0;
+}
 
-	char result[255];
+int read_write_message(int fd){
 
-	while (STOP==FALSE) {       /* loop for input */
-		res = read(fd,buf,255);   /* returns after 5 chars have been input */
-		buf[res] = 0;               /* so we can printf... */
-		printf(":%s:%d\n", buf, res);
+	int res=0;
+	char buf[255];
+	char temp[255] = {0};
+
+	STOP=FALSE;
+
+	while (STOP==FALSE) {
+		res = read(fd,buf,255);
+		buf[res] = 0;             
 		if (buf[res-1]=='\0') STOP=TRUE;
-		strcat(result,buf);
+		strcat(temp,buf);
 	}
 
-	printf("Message: %s.\n", result);
+	printf("Message: %s\n", temp);
+
 	tcflush(fd, TCIFLUSH);
 
-	res = write(fd,result,strlen(result)+1);
+	res = write(fd,temp,strlen(temp)+1);
 	printf("%d bytes written\n", res);
 
-	/*
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o
-	 */
-
-	tcsetattr(fd,TCSANOW,&oldtio);
-	close(fd);
 	return 0;
 }
--- tp1/src/writenoncanonical.c
+++ tp1/src/writenoncanonical.c
@@ -5,95 +5,138 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
+
 #define FALSE 0
 #define TRUE 1
 
 volatile int STOP=FALSE;
 
+int set_serialPort(int argc, char** argv, struct termios* oldtio, struct termios* newtio);
+int reset_serialPort(int fd, struct termios* oldtio);
+int write_message(int fd);
+int read_message(int fd);
+void atende();
+
 int main(int argc, char** argv)
 {
-	int fd,c, res;
 	struct termios oldtio,newtio;
-	char buf[255];
-	int i, sum = 0, speed = 0;
+	int error = 0;
+	//Set the serial port to right config
+	int fd=set_serialPort(argc, argv,&oldtio,&newtio);
+	if(fd != 1 || fd != -1 ){
+		printf("New termios structure set\n");
+	}else{
+		exit(fd);
+	}
+
+	//Ready do send a message
+	if((error = write_message(fd)) == -1)
+		exit(error);
+
+	//Ready to receive a message back
+	if((error = read_message(fd)) == -1)
+		exit(error);
+
+
+	if(reset_serialPort(fd,&oldtio)==-1)
+		exit(-1);
+
+	close(fd);
+	return 0;
+}
+
+int set_serialPort(int argc, char** argv, struct termios* oldtio, struct termios* newtio){
+	int fd;
 
 	if ( (argc < 2) ||
 			((strcmp("/dev/ttyS0", argv[1])!=0) &&
 					(strcmp("/dev/ttyS1", argv[1])!=0) )) {
-		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-		exit(1);
+		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS0\n");
+		return 1;
 	}
 
-
 	/*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
+	  Open serial port device for reading and writing and not as controlling tty
+	  because we don't want to get killed if linenoise sends CTRL-C.
 	 */
 
-
-	fd = open(argv[1], O_RDWR | O_NOCTTY );
-	if (fd <0) {perror(argv[1]); exit(-1); }
-
-	if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-		perror("tcgetattr");
-		exit(-1);
+	if ((fd = open(argv[1], O_RDWR | O_NOCTTY )) <0) {
+		perror(argv[1]);
+		return -1;
 	}
 
-	bzero(&newtio, sizeof(newtio));
-	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-	newtio.c_iflag = IGNPAR;
-	newtio.c_oflag = 0;
+	if ( tcgetattr(fd,oldtio) == -1) { /* save current port settings */
+		perror("tcgetattr");
+		return -1;
+	}
+
+	bzero(newtio, sizeof(*newtio));
+	(*newtio).c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	(*newtio).c_iflag = IGNPAR;
+	(*newtio).c_oflag = 0;
 
 	/* set input mode (non-canonical, no echo,...) */
-	newtio.c_lflag = 0;
+	(*newtio).c_lflag = 0;
 
-	newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-	newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+	(*newtio).c_cc[VTIME]    = 0;   /* inter-character timer unused */
+	(*newtio).c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
 	/*
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) pr�ximo(s) caracter(es)
+	  VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+	  leitura do(s) proxiimo(s) caracter(es)
 	 */
 
 	tcflush(fd, TCIOFLUSH);
 
-	if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+	if ( tcsetattr(fd,TCSANOW,newtio) == -1) {
 		perror("tcsetattr");
-		exit(-1);
+		return -1;
 	}
+	return fd;
+}
 
-	printf("New termios structure set\n");
+int reset_serialPort(int fd,struct termios* oldtio){
+	if (tcsetattr(fd,TCSANOW,oldtio) == -1) {
+		perror("tcsetattr");
+		return -1;
+	}
+	return 0;
+}
+
+int write_message(int fd){
+	char buf[255];
+	int res=0;
 
 	if(gets(buf) == NULL) {
-		perror("Error ocurred!");
-		exit(-1);
+		perror("Error ocurred on getting the message!");
+		return -1;
 	}
-
+	
 	res = write(fd,buf,strlen(buf)+1);
 	printf("%d bytes written\n", res);
 
-	/*
-    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar
-    o indicado no gui�o
-	 */
+	return 0;
+}
+
+int read_message(int fd){
 	int n = 0;
-	while (STOP==FALSE) {       /* loop for input */
-		if((res = read(fd,buf+n++,1)) == 1)
-			if (buf[n]=='\0') STOP=TRUE;
+	int res=0;
+	char result[255];
+
+	STOP=FALSE;
+	while (STOP==FALSE) {
+		res = read(fd,result+n++,1);
+			if (result[n]=='\0') STOP=TRUE;
 	}
 
-	printf("%s\n", buf);
+	printf("%s\n", result);
 	printf("%d bytes read\n", n+1);
 
-	if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-		perror("tcsetattr");
-		exit(-1);
-	}
-
-	close(fd);
 	return 0;
 }
